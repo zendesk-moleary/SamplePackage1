7228a72dc834af1411d6b0a47bca23a945cadf88
Matthew O'Leary <moleary@zendesk.com>
2020-04-21 18:09:25 +0100

    Changes to use framework

--- Sources/SamplePackage1/SamplePackage1.swift
+++ Sources/SamplePackage1/SamplePackage1.swift
@@ -1,5 +1,15 @@
+import Test1
+
 public class SamplePackage1 {
     public init() {}
 
+    public let myClass = TestClass1()
+
+
     public let greetingText: String = "Hello from package 1"
+
+    public func greeting() {
+        print("Printing from TestFrameworkOne")
+        print(myClass.testName1)
+    }
 }
